a0333729859f12a00e02f9ab4db61976dc5c1fd7
Ahmed Adel <AhmedAdel94@users.noreply.github.com>
2017-07-14 11:10:24 +0200

    Add files via upload

---
+++ Light/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Ahmed Adel on 6/13/17.
+//  Copyright © 2017 Ahmed Adel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+        lightOn = !lightOn
+        
+        updateUI()
+        
+    }
+    
+    func updateUI(){
+        view.backgroundColor = lightOn ? .white : .black
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
